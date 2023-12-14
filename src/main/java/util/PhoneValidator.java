533b9047adcf440b1ee65ff0989bf1a2b0cc534e
AdemarCastro <2022002444@ifam.edu.br>
2023-12-14 09:14:30 -0400

    [FEAT] - PhoneValidator criado e concluído no pacote Utils.

---
+++ src/main/java/util/PhoneValidator.java
@@ -0,0 +1,15 @@
+package util;
+
+public class PhoneValidator {
+
+    public void validator(Object value) {
+
+        // Garante que o formato do value seja String
+        String num = (String) value;
+
+        if (!num.matches("^\\+\\d{2} \\(\\d{2}\\) \\d{5}-\\d{4}$")) {
+
+            throw new RuntimeException("Telefone inválido, insira no formato: +DD (XX) XXXXX-XXXX.");
+        }
+    }
+}
