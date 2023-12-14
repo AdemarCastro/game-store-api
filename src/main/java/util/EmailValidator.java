740240ee12fe3f6fc887eb13fbc83e9d08c7de35
AdemarCastro <2022002444@ifam.edu.br>
2023-12-14 09:09:30 -0400

    [FEAT] - EmailValidator criado e concluído no pacote Utils.

---
+++ src/main/java/util/EmailValidator.java
@@ -0,0 +1,16 @@
+package util;
+
+public class EmailValidator {
+
+    public void validator(Object value) {
+
+        // Garante que o value seja uma String
+        String email = (String) value;
+
+        // Verifica se o email está no formato correto
+        if (!email.matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,}$")) {
+
+            throw new RuntimeException("Email inválido, insira no formato: exemplo@exemplo.com.");
+        }
+    }
+}
