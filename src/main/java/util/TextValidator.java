678c722b8dd38ed67145491e9572f42a95237624
AdemarCastro <2022002444@ifam.edu.br>
2023-12-14 09:32:41 -0400

    [REFACTOR] - Mudança do nome da classe NameValidator para TextValidator pelo fato de que irei utilizá-la para validar outros dados além de apenas o nome.

---
+++ src/main/java/util/TextValidator.java
@@ -0,0 +1,19 @@
+package util;
+
+import java.util.regex.Pattern;
+
+public class TextValidator implements Validator{
+
+    @Override
+    public void validator(Object value) {
+
+        // Garante que o name seja uma String
+        String text = (String) value;
+
+        // Validador para garantir que o text contenha penas Letras, Espaços e Acentos
+        if (!Pattern.matches("[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+", text)) {
+
+            throw new RuntimeException("Nome inválido: Somente pode conter letras, espaços e acentos.");
+        }
+    }
+}
